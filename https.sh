fc4c1d14ece02358626d8e17123459783b648fd3
George Hester <48884343+georgehester@users.noreply.github.com>
2026-03-13 12:02:57 +0000

    feat: https added

---
+++ https.sh
@@ -0,0 +1,5 @@
+mkdir -p nginx
+
+cp configuration/nginx/https.api.train.conf nginx/api.train.conf
+cp configuration/nginx/https.mapping.api.train.conf nginx/mapping.api.train.conf
+cp configuration/nginx/https.documentation.api.train.conf nginx/documentation.api.train.conf
